bf86093d87202e5a644ce2e6bd26c3b890733d4f
tirathconstacloud-max <tirath.constacloud@gmail.com>
2026-02-17 14:18:45 +0530

    Add files via upload

---
+++ my_universal_auth/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_universal_auth",
+    version="0.0.1",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+)
